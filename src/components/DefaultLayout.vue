25ee6efdaf1dce006bf6f10c6fd0be468809d72e
Alisher Nosirov <student00009474@gmail.com>
2024-02-28 12:09:44 +0100

    Changed the default page

---
+++ src/components/DefaultLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-gray-900 text-amber-50 h-full">
+    <navbar />
+    <main>
+      <router-view/>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Navbar from "./Navbar.vue";
+</script>
---
+++ src/views/MealsByName.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    By Name
+  </div>
+</template>
+
+<script setup>
+</script>
