1591b9a664ae4784295b2c8cb58953e10f93becf
Alisher Nosirov <student00009474@gmail.com>
2024-02-28 22:12:58 +0100

    Added the individual details about the meal

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import MealsByLetter from "../views/MealsByLetter.vue";
 import MealsByIngredient from "../views/MealsByIngredient.vue";
 import DefaultLayout from "../components/DefaultLayout.vue";
 import GuestLayout from "../components/GuestLayout.vue";
+import MealDetails from "../views/MealDetails.vue";
 
 const routes = [
   {
@@ -31,6 +32,11 @@ const routes = [
         path: '/by-name/:name?',
         name: 'byName',
         component: MealsByName
+      },
+      {
+        path: '/meal/:id',
+        name: 'mealDetails',
+        component: MealDetails
       }
     ]
   },
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,34 +1,52 @@
 <template>
-  <div class="p-8">
+  <div class="p-8 pb-0">
     <input type="text"
-           v-model = "keyword"
+           v-model="keyword"
            class="rounded border-2 border-gray-200 w-full bg-gray-800"
            placeholder="Search for Meals"
-           @change = "searchMeals"
+           @change="searchMeals"
     />
   </div>
 
   <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
     <div v-for="meal of meals" :key="meal.idMeal" class="bg-gray-800 shadow rounded-xl">
-      <img :src="meal.strMealThumb" :alt="strMeal" class="rounded-t-xl h-48 object-cover w-full">
-      <h3 class="p-3 font-semibold">{{meal.strMeal}}</h3>
       <div class="p-3">
-        <a :href="meals.strYoutube" target="_blank">Youtube</a>
-        <router-link to="/">View</router-link>
+       <router-link :to="{name: 'mealDetails', params:{id:meal.idMeal}}">
+         <img :src="meal.strMealThumb" :alt="strMeal" class="rounded-t-xl h-48 object-cover w-full">
+       </router-link>
+        <h3 class="font-bold">{{ meal.strMeal }}</h3>
+        <p class="mb-4">Lorem</p>
+        <div>
+          <a :href="meals.strYoutube" target="_blank"
+             class="my-2 px-3 py-2 rounded border-2 border-red-500 bg-red-500 hover:bg-red-600 transition-colors hover:cursor-pointer">
+            Youtube
+          </a>
+
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import {computed, ref} from "vue";
+import {computed, onMounted, ref} from "vue";
 import axios from "axios";
 import axiosClient from "../axiosClient.js";
 import store from "../store/index.js";
+import {useRoute} from "vue-router";
 
-const keyword= ref('')
+const route = useRoute();
+const keyword = ref('');
 const meals = computed(() => store.state.searchedMeals);
-function searchMeals(){
+
+function searchMeals() {
   store.dispatch('searchMeals', keyword.value)
 }
+
+onMounted(() =>{
+  keyword.value = route.params.name
+  if(keyword.value){
+    searchMeals();
+  }
+})
 </script>
