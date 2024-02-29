8f0895f1f291691ecf96d1feddd8e4d567e516fe
Alisher Nosirov <student00009474@gmail.com>
2024-02-29 10:24:01 +0100

    Added Search by Letter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,17 +5,6 @@
 
     </div>
 
-    <div class="flex justify-center gap-2 mt-3">
-      <router-link
-        :to="{name: 'byLetter', params:{letter}}"
-        v-for="letter of letters"
-        :key="letter">
-        {{
-          letter
-        }}
-      </router-link>
-    </div>
-
     <pre>{{ingredients}}</pre>
   </div>
 </template>
@@ -26,7 +15,7 @@ import {computed, onMounted, ref} from "vue";
 import store from "../store/index.js";
 import axiosClient from "../axiosClient.js";
 
-const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("")
+
 const ingredients = ref([])
 onMounted(async () => {
   const response = await axiosClient.get('/list.php?i=list');
